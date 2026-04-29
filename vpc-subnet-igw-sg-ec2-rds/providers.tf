427c68d04f103ce66f85b0d33cf899876347417e
PouemiArthur <pouemijunior@gmail.com>
2026-04-29 17:59:43 +0100

    refreshed repo state

--- vpc-subnet-igw-sg-ec2-rds/providers.tf
+++ vpc-subnet-igw-sg-ec2-rds/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~>5.0"
       }
    }
+backend "s3" {
+  bucket = "pjr-terraform-state-050426"
+  key = "dev2/terraform.tfstate"
+  region = "us-east-1"
+  dynamodb_table = "PJr-terraform-lock"
+  encrypt = true
+  }
 }
 
 provider "aws" {
